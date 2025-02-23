5924e595398935e78186b16a463457f6a8cce1fb
danisandami <159119950+danisandami@users.noreply.github.com>
2025-02-23 20:07:52 +0100

    resolucion AreaCalculator.java
    Añadidas variables e impresion para mostrar los valores

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -1,22 +1,27 @@
 public class AreaCalculator {
     public static void main(String[] args) {
         System.out.println("Calculadora de área para diferentes figuras.");
+
+        double circle = circleArea(5);
+        double rectangle = rectangleArea(4, 6);
+        double triangle = triangleArea(3, 7);
+
+        System.out.println("Área del círculo: " + circle);
+        System.out.println("Área del rectángulo: " + rectangle);
+        System.out.println("Área del triángulo: " + triangle);
     }
 
     public static double circleArea(double radius) {
         return Math.PI * radius * radius;
     }
 
-
-    // Añadir métodos para calcular el área de un rectángulo y un triángulo
-    
-    // Método para calcular un triángulo
+    // método Iman triangulo
     public static double triangleArea(double base, double height) { 
         return (base * height) / 2; 
     } 
 
-    
+    // método rectangulo Daniel
     public static double rectangleArea(double length, double width) {
         return length * width;
-
+    }
 }
